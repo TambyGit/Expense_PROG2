f7dd6f39be582c74a344f39aec385b273370a36e
Tojoniaina Randrianasoa <tojoniaina@gmail.com>
2025-07-31 11:00:40 +0300

    Class Expense

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+        public static int sum(int a, int b) {
+            return a + b;
+        }
+
+        public static int multiply (int a, int b) {
+            return a * b;
+        }
+    }
